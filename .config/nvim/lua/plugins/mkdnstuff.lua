0ae561134bb0eb951f5c2eccd5e119d743a5cb6c
Zachary <zacharyblundell@live.com>
2025-05-02 13:01:42 +0200

    added new plugins

---
+++ .config/nvim/lua/plugins/mkdnstuff.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "jakewvincent/mkdnflow.nvim",
+    config = function()
+      require("mkdnflow").setup({
+        -- Config goes here; leave blank for defaults
+      })
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "npm install",
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+}
